ff72bff36f829fd1e1309ada7b1491980a49aae3
thambv <94761355+thambv@users.noreply.github.com>
2024-11-14 09:51:33 -0300

    Create questao02.py

---
+++ questao02.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Localização das Comunidades Quilombolas (2022)')
+df.pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
+
