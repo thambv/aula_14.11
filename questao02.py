847c70ac162ef95d368cb27469bef866808d12f1
thambv <94761355+thambv@users.noreply.github.com>
2024-11-14 09:53:52 -0300

    Update questao02.py

--- questao02.py
+++ questao02.py
@@ -4,3 +4,15 @@ import pandas as pd
 st.title('Localização das Comunidades Quilombolas (2022)')
 df=pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
 
+df.drop(columns=['Unnamed: 0'], inplace=True)
+list = ['Lat_d', 'Long_d']
+# convertendo para numeros
+df[list] = df[list].apply(pd.to_numeric, errors='coerce')
+estados = df['NM_UF'].unique()
+estadoFiltro = st.selectbox(
+    'Qual estado selecionar?',
+     estados)
+dadosFiltrados = df[df['NM_UF'] == estadoFiltro]
+if st.checkbox('Mostrar tabela'):
+  st.write(dadosFiltrados)
+st.map(dadosFiltrados, latitude="Lat_d", longitude="Long_d")
