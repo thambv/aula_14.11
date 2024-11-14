fccf24a79eed0bb1827c3d91b6ac9f7aad8f69e9
thambv <94761355+thambv@users.noreply.github.com>
2024-11-14 09:54:48 -0300

    Update questao02.py

--- questao02.py
+++ questao02.py
@@ -16,3 +16,9 @@ dadosFiltrados = df[df['NM_UF'] == estadoFiltro]
 if st.checkbox('Mostrar tabela'):
   st.write(dadosFiltrados)
 st.map(dadosFiltrados, latitude="Lat_d", longitude="Long_d")
+
+qtdeMunicipios = len(df['NM_MUNIC'].unique())
+st.write("A quantidade de municípios com localização quilombola é " + str(qtdeMunicipios))
+
+qtdeComunidades = len(df['NM_AGLOM'].unique())
+st.write("A quantidade de comunidades quilombolas é " + str(qtdeComunidades))
